afaa10cdbf3df6ca04e4365b54087a2e29d37b2d
Danny Bozbay <107803920+dbozbay@users.noreply.github.com>
2026-02-09 15:04:00 +0000

    fix(run): add Pacman class to Game controller object

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import pygame
 from vim_man.constants import SCREENSIZE, BLACK
-from pygame.locals import QUIT
+from vim_man.pacman import Pacman
 
 
 class GameController(object):
@@ -8,6 +8,7 @@ class GameController(object):
         pygame.init()
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
+        self.clock = pygame.time.Clock()
 
     def set_background(self) -> None:
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
@@ -15,17 +16,22 @@ class GameController(object):
 
     def start_game(self) -> None:
         self.set_background()
+        self.pacman = Pacman()
 
     def update(self) -> None:
+        dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
         self.check_events()
         self.render()
 
     def check_events(self) -> None:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 exit()
 
     def render(self) -> None:
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.render(self.screen)
         pygame.display.update()
 
 
